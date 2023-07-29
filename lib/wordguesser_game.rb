c861829ca7750befee2a3e6a592520892697ca69
MesWinDu <math7064@gmail.com>
2023-07-29 15:20:29 +0000

    FinishedJubJub

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,25 +1,47 @@
 class WordGuesserGame
-
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
-  def self.get_random_word
-    require 'uri'
-    require 'net/http'
-    uri = URI('http://randomword.saasbook.info/RandomWord')
-    Net::HTTP.new('randomword.saasbook.info').start { |http|
-      return http.post(uri, "").body
-    }
+  attr_reader :word, :guesses, :wrong_guesses
+
+  def guess(letter)
+    raise ArgumentError, 'Invalid input' unless letter.is_a?(String) && letter.match?(/[a-z]/i)
+
+    letter.downcase!
+
+    if @word.include?(letter)
+      return false if @guesses.include?(letter)
+
+      @guesses << letter
+    else
+      return false if @wrong_guesses.include?(letter)
+
+      @wrong_guesses << letter
+    end
+
+    true
   end
 
+  def word_with_guesses
+    display = ''
+    @word.each_char do |char|
+      if @guesses.include?(char)
+        display << char
+      else
+        display << '-'
+      end
+    end
+    display
+  end
+
+  def check_win_or_lose
+    return :win if @word.chars.all? { |char| @guesses.include?(char) }
+    return :lose if @wrong_guesses.length >= 7
+
+    :play
+  end
 end
+
